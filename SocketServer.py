d7175f994479f7cf4598fd20eea1680a0105ac59
多双升 <liegu0317@qq.com>
2023-11-20 19:15:38 +0800

    feat(SocketServer): 修改为多线程，支持多设备同时连接
    Signed-off-by: 多双升 <liegu0317@qq.com>

--- SocketServer.py
+++ SocketServer.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import os
 import socket
+import threading
 import time
 import traceback
 
@@ -33,12 +34,25 @@ console_logger.addHandler(console_handler)
 
 
 def str2bool(v):
+    if v is None:
+        # 当传入None时，可以选择返回False或者抛出异常
+        return False  # 或者 raise argparse.ArgumentTypeError("None value is not supported.")
+
+    # 去除字符串两端的空格
+    v = v.strip()
+
+    # 处理常见的True值
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
+    # 处理常见的False值
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
         return False
+    # 处理空字符串或者其他不支持的值
+    elif not v:
+        # 选择返回False或者抛出异常
+        return False  # 或者 raise argparse.ArgumentTypeError("Empty string is not a valid boolean value.")
     else:
-        raise argparse.ArgumentTypeError('Unsupported value encountered.（遇到不支持的值。）')
+        raise argparse.ArgumentTypeError(f"Unsupported value encountered: '{v}'（遇到不支持的值：'{v}'）")
 
 
 def parse_args():
@@ -72,10 +86,9 @@ def parse_args():
 class Server():
     def __init__(self, args):
         # 服务器初始化
-        self.addr = None  # 连接地址
-        self.conn = None  # 连接对象
         logging.info('Initializing Server...')  # 初始化日志记录
 
+        # 获取对外IP
         try:
             response = requests.get('https://httpbin.org/ip')
             ip_data = response.json()
@@ -90,8 +103,6 @@ class Server():
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 创建 TCP socket 对象
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 10240000)  # 设置 socket 缓冲区大小
         self.s.bind((self.host, self.port))  # 将服务器绑定到指定的地址和端口
-        self.tmp_recv_file = 'tmp/server_received.wav'  # 临时接收文件路径
-        self.tmp_proc_file = 'tmp/server_processed.wav'  # 临时处理文件路径
 
         # 硬编码的角色映射
         self.char_name = {
@@ -122,73 +133,96 @@ class Server():
         # 情感分析引擎
         self.sentiment = SentimentEngine.SentimentEngine('SentimentEngine/models/paimon_sentiment.onnx')
 
+        logging.info("正在使用的主机IP地址：%s ", self.local_host)
+        logging.info(f"服务器正在监听 {self.host}:{self.port}...")  # 记录日志，显示服务器正在监听的地址和端口
+
     def listen(self):
         # 主服务器循环
+        self.s.listen()
         while True:
-            self.s.listen()  # 监听连接请求
-            logging.info("正在使用的主机IP地址：%s ", self.local_host)
-            logging.info(f"服务器正在监听 {self.host}:{self.port}...")  # 记录日志，显示服务器正在监听的地址和端口
-            self.conn, self.addr = self.s.accept()  # 接受客户端连接
-            logging.info(f"已连接 {self.addr}")  # 记录日志，显示已连接的客户端地址
-            self.conn.sendall(b'%s' % self.char_name[args.character][2].encode())  # 向客户端发送角色名称
+            conn, addr = self.s.accept()
+            # 为每个客户端连接创建一个新的线程
+            client_thread = threading.Thread(target=self.handle_client, args=(conn, addr))
+            client_thread.start()
+
+    def handle_client(self, conn, addr):
+        # 使用客户端地址和端口生成唯一的文件名
+        addr_str = addr[0].replace('.', '_') + '_' + str(addr[1])
+        # timestamp = int(time.time() * 1000)  # 获取毫秒级时间戳
+        # tmp_recv_file = f'tmp/{addr_str}_{timestamp}_server_received.wav'
+        # tmp_proc_file = f'tmp/{addr_str}_{timestamp}_server_processed.wav'
+        tmp_recv_file = f'tmp/{addr_str}_server_received.wav'
+        tmp_proc_file = f'tmp/{addr_str}_server_processed.wav'
+        try:
+            local_conn, local_addr = conn, addr  # 接受客户端连接
+            logging.info(f"已连接 {local_addr}")  # 记录日志，显示已连接的客户端地址
+            local_conn.sendall(b'%s' % self.char_name[args.character][2].encode())  # 向客户端发送角色名称
             while True:
                 try:
-                    file = self.__receive_file()  # 接收文件
+                    file = self.__receive_file(conn)  # 接收文件
                     logging.info('file received.')
-                    with open(self.tmp_recv_file, 'wb') as f:
+                    with open(tmp_recv_file, 'wb') as f:
                         f.write(file)
                         logging.info('已接收并保存 WAV 文件。')
-                    ask_text = self.process_voice()  # 处理语音获取文本
+                    ask_text = self.process_voice(tmp_recv_file)  # 处理语音获取文本
 
                     if args.stream:  # 流式回复
                         if "Y" in args.model or "E" in args.model:  # ERNIEBot
                             # text_generator = self.ERNIEBot.ask_stream(ask_text)  # 进行ERNIEBot对话生成
                             for resp_text in self.ERNIEBot.ask_stream(ask_text):  # 进行ERNIEBot对话生成:
-                                self.send_voice(resp_text)  # 发送语音回复
-                            self.notice_stream_end()  # 通知流式对话结束
+                                self.send_voice(resp_text, conn, tmp_proc_file)  # 发送语音回复
+                            self.notice_stream_end(conn)  # 通知流式对话结束
                             continue
                         # 以流式方式进行对话生成
                         for sentence in self.chat_gpt.ask_stream(ask_text):  # gpt
-                            self.send_voice(sentence)  # 发送语音回复
-                        self.notice_stream_end()  # 通知流式对话结束
+                            self.send_voice(sentence, conn, tmp_proc_file)  # 发送语音回复
+                        self.notice_stream_end(conn)  # 通知流式对话结束
                         logging.info('流式对话已完成。')
                     elif "Y" in args.model or "E" in args.model:  # ERNIEBot ask
                         for sentence in self.ERNIEBot.ask(ask_text):  # 进行ERNIEBot对话生成
-                            self.send_voice(sentence)  # 发送语音回复
-                        self.notice_stream_end()  # 通知流式对话结束
+                            self.send_voice(sentence, conn, tmp_proc_file)  # 发送语音回复
+                        self.notice_stream_end(conn)  # 通知流式对话结束
                         continue
                     else:  # gpt ask
                         resp_text = self.chat_gpt.ask(ask_text)  # 进行对话生成
-                        self.send_voice(resp_text)  # 发送语音回复
-                        self.notice_stream_end()  # 通知流式对话结束
+                        self.send_voice(resp_text, conn, tmp_proc_file)  # 发送语音回复
+                        self.notice_stream_end(conn)  # 通知流式对话结束
+                except ConnectionAbortedError:
+                    logging.info(f"客户端 {local_addr} 已离线")
+                    break
                 except revChatGPT.typings.APIConnectionError as e:
                     logging.error(e.__str__())
                     logging.info('API 请求频率超过限制，发送: %s' % GPT.tune.exceed_reply)
-                    self.send_voice(GPT.tune.exceed_reply, 2)  # 发送频率超过限制的语音回复
-                    self.notice_stream_end()  # 通知流式对话结束
+                    self.send_voice(GPT.tune.exceed_reply, conn, tmp_proc_file, 2)  # 发送频率超过限制的语音回复
+                    self.notice_stream_end(conn)  # 通知流式对话结束
                 except revChatGPT.typings.Error as e:
                     logging.error(e.__str__())
                     logging.info('OPENAI 出现问题，发送: %s' % GPT.tune.error_reply)
-                    self.send_voice(GPT.tune.error_reply, 1)  # 发送 OPENAI 出错的语音回复
-                    self.notice_stream_end()  # 通知流式对话结束
+                    self.send_voice(GPT.tune.error_reply, conn, tmp_proc_file, 1)  # 发送 OPENAI 出错的语音回复
+                    self.notice_stream_end(conn)  # 通知流式对话结束
                 except requests.exceptions.RequestException as e:
                     logging.error(e.__str__())
                     logging.info('网络出现问题，发送: %s' % GPT.tune.error_reply)
-                    self.send_voice(GPT.tune.error_reply, 1)  # 发送网络出错的语音回复
-                    self.notice_stream_end()  # 通知流式对话结束
+                    self.send_voice(GPT.tune.error_reply, conn, tmp_proc_file, 1)  # 发送网络出错的语音回复
+                    self.notice_stream_end(conn)  # 通知流式对话结束
                 except Exception as e:
                     logging.error(e.__str__())
                     logging.error(traceback.format_exc())
                     break
+        except Exception as e:
+            logging.error(f"Error in thread handling client {addr}: {e}")
+            # logging.error(e.__str__())
+            # logging.error(traceback.format_exc())
 
-    def notice_stream_end(self):
+    @staticmethod
+    def notice_stream_end(conn):
         """
         通知流式对话结束的方法。
         """
         time.sleep(0.5)
-        self.conn.sendall(b'stream_finished')  # 向客户端发送流式对话结束的通知
+        conn.sendall(b'stream_finished')  # 向客户端发送流式对话结束的通知
 
-    def send_voice(self, resp_text, senti_or=None):
+    def send_voice(self, resp_text, conn, tmp_proc_file, senti_or=None):
         """
         发送语音回复的方法。
 
@@ -199,8 +233,8 @@ class Server():
         如果指定了情感分析结果（senti_or），则使用指定的情感值发送语音回复；
         否则，根据文本内容进行情感分析并发送语音回复。
         """
-        self.tts.read_save(resp_text, self.tmp_proc_file, self.tts.hps.data.sampling_rate)  # 将回复文本转换为语音并保存为临时处理文件
-        with open(self.tmp_proc_file, 'rb') as f:
+        self.tts.read_save(resp_text, tmp_proc_file, self.tts.hps.data.sampling_rate)  # 将回复文本转换为语音并保存为临时处理文件
+        with open(tmp_proc_file, 'rb') as f:
             senddata = f.read()
         if senti_or:
             senti = senti_or
@@ -208,11 +242,12 @@ class Server():
             senti = self.sentiment.infer(resp_text)  # 对回复文本进行情感分析
         senddata += b'?!'
         senddata += b'%i' % senti
-        self.conn.sendall(senddata)  # 向客户端发送语音回复
+        conn.sendall(senddata)  # 向客户端发送语音回复
         time.sleep(0.5)
         logging.info('WAV SENT, size %i' % len(senddata))  # 记录发送的语音回复的大小
 
-    def __receive_file(self):
+    @staticmethod
+    def __receive_file(conn):
         """
         接收文件的私有方法。
 
@@ -220,9 +255,9 @@ class Server():
         """
         file_data = b''
         while True:
-            data = self.conn.recv(1024)
+            data = conn.recv(1024)
             # print(data)
-            self.conn.send(b'sb')
+            conn.send(b'sb')
             if data[-2:] == b'?!':
                 file_data += data[0:-2]
                 break
@@ -233,15 +268,15 @@ class Server():
 
         return file_data
 
-    def fill_size_wav(self):
+    def fill_size_wav(self, tmp_recv_file):
         """
         填充 WAV 文件的大小。
 
         将文件大小信息写入 WAV 文件的相应位置。
         """
-        with open(self.tmp_recv_file, "r+b") as f:
+        with open(tmp_recv_file, "r+b") as f:
             # 获取文件的大小
-            size = os.path.getsize(self.tmp_recv_file) - 8
+            size = os.path.getsize(tmp_recv_file) - 8
             # 将文件大小写入前4个字节
             f.seek(4)
             f.write(size.to_bytes(4, byteorder='little'))
@@ -249,19 +284,19 @@ class Server():
             f.write((size - 28).to_bytes(4, byteorder='little'))
             f.flush()
 
-    def process_voice(self):
+    def process_voice(self, tmp_recv_file):
         """
         处理语音的方法。
 
         返回语音转换为文本后的结果。
         """
         # 将立体声转换为单声道
-        self.fill_size_wav()
-        y, sr = librosa.load(self.tmp_recv_file, sr=None, mono=False)
+        self.fill_size_wav(tmp_recv_file)
+        y, sr = librosa.load(tmp_recv_file, sr=None, mono=False)
         y_mono = librosa.to_mono(y)
         y_mono = librosa.resample(y_mono, orig_sr=sr, target_sr=16000)
-        soundfile.write(self.tmp_recv_file, y_mono, 16000)
-        text = self.paraformer.infer(self.tmp_recv_file)  # 将语音转换为文本
+        soundfile.write(tmp_recv_file, y_mono, 16000)
+        text = self.paraformer.infer(tmp_recv_file)  # 将语音转换为文本
 
         return text
 
